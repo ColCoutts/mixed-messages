7f165a1d9b4ed96c39f9ee5ed4333af24487c5e4
Colin Coutts <colintcoutts@gmail.com>
2021-09-04 15:12:45 -0700

    flushing out MVP for this project

---
+++ index.js
@@ -0,0 +1,8 @@
+//NOTES
+/*
+  this will be a star trek message generator. MVP is currently slated to be an array of trek quotes that load each time index.js gets run. This is fairly simplistic so I'm trying to think of ways to spice this up. Some thoughts:
+    - include the Date and time along with the quote to give it some temporal relatability?
+    - maybe turn this array into an object where each key is a different character, the values of which being their quotes
+    - maybe depending on the day you could get a different character and one of their quotes?
+
+*/
